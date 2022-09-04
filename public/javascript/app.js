3b5e6e8f46955ee6b3e1f78585608e70032cc0b1
Chima <onyiaguchima@gmail.com>
2022-09-04 21:29:57 +0100

    agent pid killed

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -7,5 +7,5 @@ fetch("https://api.coindesk.com/v1/bpi/currentprice.json").then((result)=>{
 const weatherform = document.querySelector("form");
 weatherform.addEventListener("submit", (e)=>{
     e.preventDefault();
-    console.log(e, "errorogram")
+    console.log(e);
 });
